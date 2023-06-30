2d57a5c384996525085a34eb113b0c2def47ec6b
Vadim-27 <vadim.bulanyi@gmail.com>
2023-06-30 14:13:16 +0200

    apiGetAll

---
+++ src/shared/services/usersApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: 'https://frontend-test-assignment-api.abz.agency',
+});
+
+export const getAllUsers = async () => {
+    const result = await instance.get(`api/v1/users`);
+    console.log('result', result)
+    return result;
+}
